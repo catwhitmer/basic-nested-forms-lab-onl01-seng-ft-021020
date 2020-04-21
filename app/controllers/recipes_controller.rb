01f07fd7eca58fd1fdc8c80477da8bfe44dc8665
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-21 13:12:44 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,6 +17,7 @@ class RecipesController < ApplicationController
   end
   
   private
+  
     def recipe_params
       params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
     end
