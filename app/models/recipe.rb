1b539d50e783636db181ddbe41e253a94b42a481
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-21 12:57:19 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients 
    accepts_nested_attributes_for :ingredients
    
-   def recipe_attributes=(artist)
+   def recipe_attributes=(recipe)
     self.artist = Artist.find_or_create_by(name: artist[:name])
     self.artist.update(artist)
   end
